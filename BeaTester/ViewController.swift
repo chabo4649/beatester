84e1810380010143eefbb509a4e187fd40190597
井口陽介 <chabo4649@gmail.com>
2017-10-30 23:54:38 +0900

    Initial Commit

---
+++ BeaTester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeaTester
+//
+//  Created by 井口陽介 on 2017/10/30.
+//  Copyright © 2017年 井口陽介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
